a9de68dfaa86b7971b376cdbec7b6c3f0e628fe9
pcualMac <jfpum.cal@gmail.com>
2021-06-07 09:20:23 +0100

    clean register()

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,17 +26,5 @@ class AppServiceProvider extends ServiceProvider
         app()->singleton('ClassValidation',function(){
             return new ClassValidation();
         });
-        // app()->singleton('validator', function ($app) {
-        //     $validator = new Factory($app['translator'], $app);
-
-        //     // The validation presence verifier is responsible for determining the existence of
-        //     // values in a given data collection which is typically a relational database or
-        //     // other persistent data stores. It is used to check for "uniqueness" as well.
-        //     if (isset($app['db'], $app['validation.presence'])) {
-        //         $validator->setPresenceVerifier($app['validation.presence']);
-        //     }
-
-        //     return $validator;
-        // });
     }
 }
